ae1d06e06683d74aec9aaac5cc5a2f57b762b614
Arif Rachman Hakim <arifrachmanhakim07@gmail.com>
2016-09-11 13:00:14 +0700

    Create aa.js

---
+++ aa.js
@@ -0,0 +1,2 @@
+document.getElementById("klik").onclick= function() {openMobilemenu()};
+function openMobilemenu(){var e=document.getElementById("mobile-menu-nav");"block"!==e.style.display?e.style.display="block":e.style.display="none"};
